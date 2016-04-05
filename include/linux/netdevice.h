23bc3a4e031aa476b00e37bfd73f5522a8e60d59
Tom Herbert <tom@herbertland.com>
2016-04-05 08:22:56 -0700

    BACKPORT: udp: Remove udp_offloads
    Now that the UDP encapsulation GRO functions have been moved to the UDP
    socket we not longer need the udp_offload insfrastructure so removing it.
    
    Change-Id: I209b34af744bd7a00975edcecd3a5b620882d75b
    Signed-off-by: Tom Herbert <tom@herbertland.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2095,40 +2095,7 @@ struct packet_offload {
 	struct list_head	 list;
 };
 
-struct udp_offload;
-
-struct udp_offload_callbacks {
-	struct sk_buff		**(*gro_receive)(struct sk_buff **head,
-						 struct sk_buff *skb,
-						 struct udp_offload *uoff);
-	int			(*gro_complete)(struct sk_buff *skb,
-						int nhoff,
-						struct udp_offload *uoff);
-};
-
-struct udp_offload {
-	__be16			 port;
-	u8			 ipproto;
-	struct udp_offload_callbacks callbacks;
-};
-
-typedef struct sk_buff **(*gro_receive_udp_t)(struct sk_buff **,
-					      struct sk_buff *,
-					      struct udp_offload *);
-static inline struct sk_buff **call_gro_receive_udp(gro_receive_udp_t cb,
-						    struct sk_buff **head,
-						    struct sk_buff *skb,
-						    struct udp_offload *uoff)
-{
-	if (unlikely(gro_recursion_inc_test(skb))) {
-		NAPI_GRO_CB(skb)->flush |= 1;
-		return NULL;
-	}
-
-	return cb(head, skb, uoff);
-}
-
-/* often modified stats are per cpu, other are shared (netdev->stats) */
+/* often modified stats are per-CPU, other are shared (netdev->stats) */
 struct pcpu_sw_netstats {
 	u64     rx_packets;
 	u64     rx_bytes;
