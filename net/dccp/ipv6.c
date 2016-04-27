7c08dbfd9c7a53b40813bf2f1e108f2e9f019a0e
Eric Dumazet <edumazet@google.com>
2016-04-27 16:44:36 -0700

    UPSTREAM: net: rename ICMP6_INC_STATS_BH()
    Rename ICMP6_INC_STATS_BH() to __ICMP6_INC_STATS()
    
    Change-Id: I68715c6d8708811ca5117052dd4aadab2eb639af
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dccp/ipv6.c
+++ net/dccp/ipv6.c
@@ -92,8 +92,8 @@ static void dccp_v6_err(struct sk_buff *skb, struct inet6_skb_parm *opt,
 					inet6_iif(skb));
 
 	if (!sk) {
-		ICMP6_INC_STATS_BH(net, __in6_dev_get(skb->dev),
-				   ICMP6_MIB_INERRORS);
+		__ICMP6_INC_STATS(net, __in6_dev_get(skb->dev),
+				  ICMP6_MIB_INERRORS);
 		return;
 	}
 
