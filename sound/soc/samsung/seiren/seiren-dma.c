1a6f92771a0248caadb4d48d5dc18f7cc2793bb5
nhglong <its.nhglong@gmail.com>
2026-01-25 11:18:57 +0700

    Revert "[COMMON] ASoC: samsung: remove code related to seiren-dma"
    This reverts commit 6b5aca3e2ffa0a0cce9d0ce1554ed821ea525a14.

--- sound/soc/samsung/seiren/seiren-dma.c
+++ sound/soc/samsung/seiren/seiren-dma.c
@@ -27,6 +27,7 @@
 #include <sound/exynos.h>
 
 #include <asm/dma.h>
+#include <mach/map.h>
 
 #include "seiren.h"
 #include "seiren-dma.h"
@@ -267,7 +268,7 @@ static int esa_dma_probe(struct platform_device *pdev)
 		return -ENXIO;
 	}
 
-	sdi.regs = devm_ioremap_resource(&pdev->dev, res);
+	sdi.regs = devm_request_and_ioremap(&pdev->dev, res);
 	if (!sdi.regs) {
 		dev_err(dev, "SFR ioremap failed\n");
 		return -ENOMEM;
