c7b8c1dd6bb0ff995c7844dd3d3c08c26f7b88f8
nhglong <its.nhglong@gmail.com>
2026-01-25 10:47:41 +0700

    Revert "[8890] ASoC: samsung: add cp_status check"
    This reverts commit 38971ac2985a73d10d2d8da9f89097a16c0de96b.

--- sound/soc/samsung/lucky_arizona_lsi.c
+++ sound/soc/samsung/lucky_arizona_lsi.c
@@ -155,8 +155,6 @@ struct arizona_machine_priv {
 #endif
 };
 
-extern void update_cp_available(bool cpen);
-
 static struct class *svoice_class;
 static struct device *keyword_dev;
 static unsigned int keyword_type;
@@ -964,8 +962,6 @@ static int lucky_aif2_hw_params(struct snd_pcm_substream *substream,
 		}
 	}
 
-	update_cp_available(true);
-
 	return 0;
 }
 
@@ -983,8 +979,6 @@ static int lucky_aif2_hw_free(struct snd_pcm_substream *substream)
 	priv->asyncclk.fll_ref_out = 0;
 	memcpy(&priv->asyncclk.mclk, &lucky_mclk_data[0],
 					sizeof(struct snd_mclk_info));
-
-	update_cp_available(false);
 
 	return 0;
 }
