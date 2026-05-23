fce55bfd9ff7794b3b56433fd73a34b49813ba53
nhglong <its.nhglong@gmail.com>
2026-05-23 20:18:14 +0700

    drivers: staging: samsung: Remove SEC_DEBUG_FILE_LEAK
    Change-Id: I129ef654313f18fff4a45bdcc9ce2b8525e81903

--- fs/file.c
+++ fs/file.c
@@ -35,10 +35,6 @@ int sysctl_nr_open_min = BITS_PER_LONG;
 int sysctl_nr_open_max = __const_max(INT_MAX, ~(size_t)0/sizeof(void *)) &
 			 -BITS_PER_LONG;
 
-#ifdef CONFIG_SEC_DEBUG_FILE_LEAK
-extern void	sec_debug_EMFILE_error_proc(unsigned long files_addr);
-#endif
-
 static void *alloc_fdmem(size_t size)
 {
 	/*
@@ -197,9 +193,6 @@ static int expand_fdtable(struct files_struct *files, int nr)
 	 * caller and alloc_fdtable().  Cheaper to catch it here...
 	 */
 	if (unlikely(new_fdt->max_fds <= nr)) {
-#ifdef CONFIG_SEC_DEBUG_FILE_LEAK
-		sec_debug_EMFILE_error_proc((unsigned long)files);
-#endif
 		__free_fdtable(new_fdt);
 		return -EMFILE;
 	}
@@ -238,9 +231,6 @@ repeat:
 
 	/* Can we expand? */
 	if (nr >= sysctl_nr_open) {
-#ifdef CONFIG_SEC_DEBUG_FILE_LEAK
-		sec_debug_EMFILE_error_proc((unsigned long)files);
-#endif
 		return -EMFILE;
 	}
 
@@ -351,9 +341,6 @@ struct files_struct *dup_fd(struct files_struct *oldf, int *errorp)
 
 		/* beyond sysctl_nr_open; nothing to do */
 		if (unlikely(new_fdt->max_fds < open_files)) {
-#ifdef CONFIG_SEC_DEBUG_FILE_LEAK
-			sec_debug_EMFILE_error_proc((unsigned long)oldf);
-#endif
 			__free_fdtable(new_fdt);
 			*errorp = -EMFILE;
 			goto out_release;
@@ -539,9 +526,6 @@ repeat:
 	 */
 	error = -EMFILE;
 	if (fd >= end) {
-#ifdef CONFIG_SEC_DEBUG_FILE_LEAK
-		sec_debug_EMFILE_error_proc((unsigned long)files);
-#endif
 		goto out;
 	}
 
@@ -930,9 +914,6 @@ SYSCALL_DEFINE3(dup3, unsigned int, oldfd, unsigned int, newfd, int, flags)
 		return -EINVAL;
 
 	if (newfd >= rlimit(RLIMIT_NOFILE)) {
-#ifdef CONFIG_SEC_DEBUG_FILE_LEAK
- 		sec_debug_EMFILE_error_proc((unsigned long)files);
-#endif
 		return -EBADF;
 	}
 
