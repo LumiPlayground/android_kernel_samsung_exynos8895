c810e48f33c10a1e978bb7715d8abd8ace347d4d
nhglong <its.nhglong@gmail.com>
2026-06-27 21:47:24 +0700

    drivers: gpu: arm: Adapt get_user_pages after 4.4.168 merge
    Change-Id: I32b6e2c693c88a67ce449e01c55f90f56c64c60a

--- drivers/gpu/arm/b_r16p0/mali_kbase_mem_linux.c
+++ drivers/gpu/arm/b_r16p0/mali_kbase_mem_linux.c
@@ -1124,14 +1124,18 @@ static struct kbase_va_region *kbase_mem_from_user_buffer(
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 6, 0)
 	faulted_pages = get_user_pages(current, current->mm, address, *va_pages,
+#if KERNEL_VERSION(4, 4, 168) <= LINUX_VERSION_CODE && \
+KERNEL_VERSION(4, 5, 0) > LINUX_VERSION_CODE
+			write ? FOLL_WRITE : 0, pages, NULL);
+#else
 			write, 0, pages, NULL);
+#endif
 #elif LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0)
 	faulted_pages = get_user_pages(address, *va_pages,
 			write, 0, pages, NULL);
 #else
 	faulted_pages = get_user_pages(address, *va_pages,
-			write ? FOLL_WRITE : 0,
-			pages, NULL);
+			write ? FOLL_WRITE : 0, pages, NULL);
 #endif
 
 	up_read(&current->mm->mmap_sem);
